393b0d4915b7dd55314545bfe05d4a9ee0434f0a
TH <kareme007@gmail.com>
2024-10-18 16:32:49 +0900

    2024-10-18

--- src/KakaoMap.js
+++ src/KakaoMap.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { Map, MapTypeControl, MapMarker, CustomOverlayMap } from "react-kakao-maps-sdk";
+import {
+  Map,
+  MapTypeControl,
+  MapMarker,
+  CustomOverlayMap,
+  ZoomControl,
+} from "react-kakao-maps-sdk";
 import { useState, useRef } from "react";
-import Info from "./Info";
 
 function KakaoMap(props) {
   const [coordinate, setCoordinate] = useState({
@@ -11,56 +16,55 @@ function KakaoMap(props) {
     isPanto: false,
   });
 
-  const [isOpen, setIsOpen] = useState(false)
-  const mapRef = useRef(null)
-  const [info, setInfo] = useState("")
+  const mapRef = useRef(null);
+  const [info, setInfo] = useState("");
 
   const getInfo = () => {
-    const map = mapRef.current
+    const map = mapRef.current;
     console.log(mapRef);
     console.log(map);
-    if (!map) return
+    if (!map) return;
 
-    const center = map.getCenter()
+    const center = map.getCenter();
 
     // 지도의 현재 레벨을 얻어옵니다
-    const level = map.getLevel()
+    const level = map.getLevel();
 
     // 지도타입을 얻어옵니다
-    const mapTypeId = map.getMapTypeId()
+    const mapTypeId = map.getMapTypeId();
 
     // 지도의 현재 영역을 얻어옵니다
-    const bounds = map.getBounds()
+    const bounds = map.getBounds();
 
     // 영역의 남서쪽 좌표를 얻어옵니다
-    const swLatLng = bounds.getSouthWest()
+    const swLatLng = bounds.getSouthWest();
 
     // 영역의 북동쪽 좌표를 얻어옵니다
-    const neLatLng = bounds.getNorthEast()
+    const neLatLng = bounds.getNorthEast();
 
     // 영역정보를 문자열로 얻어옵니다. ((남,서), (북,동)) 형식입니다
     // const boundsStr = bounds.toString()
 
-    let message = "지도 중심좌표는 위도 " + center.getLat() + ", <br>"
-    message += "경도 " + center.getLng() + " 이고 <br>"
-    message += "지도 레벨은 " + level + " 입니다 <br> <br>"
-    message += "지도 타입은 " + mapTypeId + " 이고 <br> "
+    let message = "지도 중심좌표는 위도 " + center.getLat() + ", <br>";
+    message += "경도 " + center.getLng() + " 이고 <br>";
+    message += "지도 레벨은 " + level + " 입니다 <br> <br>";
+    message += "지도 타입은 " + mapTypeId + " 이고 <br> ";
     message +=
       "지도의 남서쪽 좌표는 " +
       swLatLng.getLat() +
       ", " +
       swLatLng.getLng() +
-      " 이고 <br>"
+      " 이고 <br>";
     message +=
       "북동쪽 좌표는 " +
       neLatLng.getLat() +
       ", " +
       neLatLng.getLng() +
-      " 입니다"
-    setInfo(message)
+      " 입니다";
+    setInfo(message);
     console.log(info);
     console.log(message);
-  }
+  };
 
   const positions = [
     {
@@ -79,32 +83,60 @@ function KakaoMap(props) {
       title: "근린공원",
       latlng: { lat: 33.451393, lng: 126.570738 },
     },
-  ]
+  ];
+
+  const SetMapMaker = ( props ) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+      <MapMarker // 인포윈도우를 생성하고 지도에 표시합니다
+        position={props.position}
+        clickable={true} // 마커를 클릭했을 때 지도의 클릭 이벤트가 발생하지 않도록 설정합니다
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        {/* MapMarker의 자식을 넣어줌으로 해당 자식이 InfoWindow로 만들어지게 합니다 */}
+        {/* 인포윈도우에 표출될 내용으로 HTML 문자열이나 React Component가 가능합니다 */}
+        {isOpen && (
+          <div style={{ minWidth: "150px" }}>
+            <img
+              alt="close"
+              width="14"
+              height="13"
+              src="https://t1.daumcdn.net/localimg/localimages/07/mapjsapi/2x/bt_close.gif"
+              style={{
+                position: "absolute",
+                right: "5px",
+                top: "5px",
+                cursor: "pointer",
+              }}
+              onClick={() => setIsOpen(false)}
+            />
+            <div>날짜</div>
+            <div style={{ padding: "5px", color: "#000" }}>
+              <a href="https://www.naver.com/">{props.title}</a>
+              </div>
+          </div>
+        )}
+      </MapMarker>
+    );
+  };
 
   return (
-      <Map
-        center={coordinate.center}
-        style={{ width: "100%", height: "600px" }}
-        level={3}
-        ref={mapRef}
-      >
+    <Map
+      center={coordinate.center}
+      style={{ width: "100%", height: "600px" }}
+      level={3}
+      ref={mapRef}
+    >
+      <ZoomControl />
       <MapTypeControl position={"TOPRIGHT"} />
-      {positions.map((position, index) => (
-        <MapMarker
-          key={`${position.title}-${position.latlng}`}
-          position={position.latlng} // 마커를 표시할 위치
-          image={{
-            src: "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png", // 마커이미지의 주소입니다
-            size: {
-              width: 24,
-              height: 35
-            }, // 마커이미지의 크기입니다
-          }}
-          title={position.title} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-          onClick={() => setIsOpen(true)}
+      {positions.map((value) => (
+        <SetMapMaker
+          key={`${value.title}-${value.latlng}`}
+          position={value.latlng} // 마커를 표시할 위치
+          title={value.title} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
         />
       ))}
-      {isOpen && (<Info setIsOpen={setIsOpen}/>)}
       <button
         onClick={() =>
           setCoordinate({
@@ -124,7 +156,7 @@ function KakaoMap(props) {
           __html: info,
         }}
       />
-      </Map>
+    </Map>
   );
 }
 
