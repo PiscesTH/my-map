8035c8a956425545dd7d59cc481136e88fab0c24
TH <kareme007@gmail.com>
2024-12-07 18:40:07 +0900

    2024-12-07

--- src/Location.js
+++ src/Location.js
@@ -1,23 +1,42 @@
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash, faMapLocationDot } from "@fortawesome/free-solid-svg-icons";
 
 function Location(props) {
   const [ilocation, setIlocation] = useState();
   const [title, setTitle] = useState();
   const [date, setDate] = useState();
   const [pictures, setPictures] = useState([]);
+  const [ipicture, setIpicture] = useState(0);
   const [selectedImage, setSelectedImage] = useState(null);
 
   // 모달 열기
-  const openModal = (image) => {
+  const openModal = (image, event) => {
     const str = JSON.stringify(image.thumbnails);
-    setSelectedImage(str.slice(3, str.indexOf('&quot')));
+    setSelectedImage(str.slice(3, str.indexOf("&quot")));
+    setIpicture(event.currentTarget.dataset.pk);
   };
 
   // 모달 닫기
   const closeModal = () => {
     setSelectedImage(null);
+    setIpicture(0);
+  };
+
+  const deletePicture = async (event) => {
+    const pk = event.currentTarget.dataset.pk;
+    try {
+      const res = await axios.delete(
+        `http://localhost:8080/api/location/${pk}`
+      );
+      const timer = setTimeout(() => closeModal(), 1000);
+      clearTimeout(timer);
+      window.location.reload();
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   useEffect(() => {
@@ -25,7 +44,7 @@ function Location(props) {
       try {
         const res = await axios.get("http://localhost:8080/api/location", {
           params: {
-            ilocation: 5,
+            ilocation: 6,
           },
         });
         console.log("data", res.data);
@@ -43,29 +62,40 @@ function Location(props) {
 
   return (
     <div className="location-container">
+      <NavLink className="return-button hover-red" to={"/"}>
+      <FontAwesomeIcon icon={faMapLocationDot} />
+      </NavLink>
       <h2>제목 : {title}</h2>
       <div>{date}</div>
       <h3>사진 미리보기</h3>
       {pictures.map((thumbnail, index) => (
         <img
           key={index}
+          data-pk={thumbnail.ipicture}
           src={`http://localhost:8080/location/${ilocation}/${thumbnail.thumbnails}`}
           alt={thumbnail.thumbnails}
           className="thumbnails"
-          onClick={() => openModal(thumbnail)}
+          onClick={(event) => openModal(thumbnail, event)}
         />
       ))}
       {/* 모달 */}
       {selectedImage && (
         <div className="modal">
           <div className="modal-content">
-            <span className="close-button" onClick={closeModal}>
+            <span className="close-button hover-red" onClick={closeModal}>
               &times;
+            </span>
+            <span
+              className="delete-button hover-red"
+              data-pk={ipicture}
+              onClick={(event) => deletePicture(event)}
+            >
+              <FontAwesomeIcon icon={faTrash} />
             </span>
             <img
               src={`http://localhost:8080/location/${ilocation}/${selectedImage}`}
               alt={selectedImage}
-              style={{ maxWidth: "90%", maxHeight: "90%" }}
+              className="modal-image"
             />
           </div>
         </div>
